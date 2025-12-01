98b019deb1abe10647c13749cd4a5af1af29cb61
Tom <tomgaming@hotmail.no>
2025-12-01 20:49:14 +0100

    Add project files.

---
+++ oppgave1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+var dag = 4;
+
+switch (dag)
+{
+    case 1:
+        Console.WriteLine("Mandag");
+        break;
+    case 2:
+        Console.WriteLine("Tirsdag");
+        break;
+    case 3:
+        Console.WriteLine("Onsdag");
+        break;
+    case 4:
+        Console.WriteLine("Torsdag");
+        break;
+    case 5:
+        Console.WriteLine("Fredag");
+        break;
+    case 6:
+        Console.WriteLine("Lørdag");
+        break;
+    case 7:
+        Console.WriteLine("Søndag");
+        break;
+    default:
+        Console.WriteLine("Ugyldig tall");
+        break;
+}
---
+++ switch/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Skriv en tall mellom 1 til 7:");
+
+string? input = Console.ReadLine();
+
+if (!int.TryParse(input, out int dag))
+{
+    Console.WriteLine("Ugyldig input, vennligst skriv et tall mellom 1 og 7.");
+}
+else
+{
+    switch (dag)
+    {
+        case 1:
+            Console.WriteLine("Mandag");
+            break;
+        case 2:
+            Console.WriteLine("Tirsdag");
+            break;
+        case 3:
+            Console.WriteLine("Onsdag");
+            break;
+        case 4:
+            Console.WriteLine("Torsdag");
+            break;
+        case 5:
+            Console.WriteLine("Fredag");
+            break;
+        case 6:
+            Console.WriteLine("Lørdag");
+            break;
+        case 7:
+            Console.WriteLine("Søndag");
+            break;
+        default:
+            Console.WriteLine("Ugyldig tall, vennligst skriv et tall mellom 1 og 7.");
+            break;
+    }
+}
